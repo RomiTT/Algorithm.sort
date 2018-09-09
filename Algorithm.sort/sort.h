6bf04904c91ac6e5a32e95193853546bac394c43
RomiTT <chikyom.kim@gmail.com>
2018-09-09 19:11:22 +0900

    Implemented selection sort, bubble sort, insertion sort, quick sort, heap sort for exercise.

--- Algorithm.sort/sort.h
+++ Algorithm.sort/sort.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <functional>
+#include <algorithm>
 #include <iostream>
 
 class SortFunctions {
@@ -22,7 +23,8 @@ public:
 	static void bubbleSort(std::vector<int>& v) {
 		for (size_t i = 0; i < v.size(); i++) {
 			for (size_t j = 0; j < v.size()- i-1; j++) {
-				if (v[j] > v[j + 1]) std::swap(v[j], v[j + 1]);
+				if (v[j] > v[j + 1]) 
+					std::swap(v[j], v[j + 1]);
 			}
 		}
 	}
@@ -30,8 +32,10 @@ public:
 	static void insertionSort(std::vector<int>& v) {
 		for (size_t i = 0; i < v.size()-1; i++) {
 			for (size_t j = i + 1; j > 0; j--) {
-				if (v[j] < v[j - 1]) std::swap(v[j], v[j - 1]);
-				else break;
+				if (v[j] < v[j - 1]) 
+					std::swap(v[j], v[j - 1]);
+				else 
+					break;
 			}
 		}
 	}
@@ -47,15 +51,21 @@ public:
 				bI = bJ = false;
 
 				while (!(bI && bJ)) {
-					if (i <= end && v[pivot] >= v[i] && !bI) i++;
-					else bI = true;
+					if (i <= end && v[pivot] >= v[i] && !bI) 
+						i++;
+					else 
+						bI = true;
 
-					if (j > start && v[pivot] <= v[j] && !bJ) j--;
-					else bJ = true;
+					if (j > start && v[pivot] <= v[j] && !bJ) 
+						j--;
+					else 
+						bJ = true;
 				}
 
-				if (i > j) std::swap(v[pivot], v[j]);
-				else std::swap(v[i], v[j]);
+				if (i > j) 
+					std::swap(v[pivot], v[j]);
+				else 
+					std::swap(v[i], v[j]);
 			}
 
 			if (start < j) qsort(v, start, j - 1);
@@ -66,10 +76,76 @@ public:
 	}
 
 	static void mergeSort(std::vector<int>& v) {
+		std::vector<int> temp(v.size());
+		auto merge = [&temp](std::vector<int>& v, size_t start, size_t mid, size_t end) {
+			if ((end - start) == 1) {
+				if (v[end] < v[start]) 
+					std::swap(v[start], v[end]);
+				return;
+			}
 
+			size_t i = start, j = mid+1, pos = start;
+			while (i <= mid && j <= end) {
+				if (v[i] < v[j]) {
+					temp[pos] = v[i]; i++;
+				}
+				else {
+					temp[pos] = v[j]; j++;
+				}
+				pos++;
+			}
+
+			if (i > mid) {
+				for (; j <= end; j++) {
+					temp[pos] = v[j]; pos++;
+				}
+			}
+			else {
+				for (; i <= mid; i++) {
+					temp[pos] = v[i]; pos++;
+				}
+			}
+
+			for (i = start; i <= end; i++) v[i] = temp[i];
+		};
+
+		std::function<void(std::vector<int>&, size_t, size_t)> sort;
+		sort = [&sort, &merge](std::vector<int>& v, size_t start, size_t end) {
+			if (start < end) {
+				int mid = (start + end) / 2;
+				sort(v, start, mid);
+				sort(v, mid + 1, end);
+				merge(v, start, mid, end);
+			}
+		};
+
+		sort(v, 0, v.size() - 1);
 	}
 
 	static void heapSort(std::vector<int>& v) {
+		for (int i = 1; i < v.size(); i++) {
+			int child = i;
+			do {
+				int root = (child - 1) / 2;
+				if (v[root] < v[child]) 
+					std::swap(v[root], v[child]);
+				else break;
+				child = root;
+			} while (child != 0);
+		}
 
+		for (int i = v.size() - 1; i >= 0 ; i--) {
+			std::swap(v[0], v[i]);
+			int root = 0, child;
+			do {
+				child = 2 * root + 1;
+				if (child < i - 1 && v[child] < v[child + 1]) 
+					child++;
+				if (child < i && v[root] < v[child]) 
+					std::swap(v[root], v[child]);
+				else break;
+				root = child;
+			} while (child < i);
+		}
 	}
 };
--- sort-test/test.cpp
+++ sort-test/test.cpp
@@ -1,108 +1,72 @@
 #include "pch.h"
 #include "../Algorithm.sort/sort.h"
 
-int getRandomNumber(int min, int max)
-{
-	//< 1단계. 시드 설정
-	random_device rn;
-	mt19937_64 rnd(rn());
 
-	//< 2단계. 분포 설정 ( 정수 )
-	uniform_int_distribution<int> range(min, max);
-
-	//< 3단계. 값 추출
-	return range(rnd);
-}
-
-const int MIN = -100, MAX = 100, SIZE = 100;
-
-
-TEST(SelectionSort, Sort) {
+class SortTest : public ::testing::Test {
+protected:
+	const int MIN = -1000, MAX = 1000, SIZE = 2000;
 	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
+
+protected:
+	void SetUp() override {
+		for (int i = 0; i < SIZE; i++) {
+			int val = getRandomNumber(MIN, MAX);
+			v1.push_back(val);
+			v2.push_back(val);
+		}
 	}
 
+	void TearDown() override {
+
+	}
+
+private:
+	int getRandomNumber(int min, int max)
+	{
+		//< 1단계. 시드 설정
+		random_device rn;
+		mt19937_64 rnd(rn());
+		//< 2단계. 분포 설정 ( 정수 )
+		uniform_int_distribution<int> range(min, max);
+		//< 3단계. 값 추출
+		return range(rnd);
+	}
+};
+
+
+
+TEST_F(SortTest, selectionSort) {
 	SortFunctions::selectionSort(v1);
 	std::sort(v2.begin(), v2.end());
   EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
 
-TEST(BubbleSort, Sort) {
-	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
-	}
-
+TEST_F(SortTest, BubbleSort) {
 	SortFunctions::bubbleSort(v1);
 	std::sort(v2.begin(), v2.end());
 	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
-TEST(InsertionSort, Sort) {
-	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
-	}
-
+TEST_F(SortTest, InsertionSort) {
 	SortFunctions::insertionSort(v1);
 	std::sort(v2.begin(), v2.end());
 	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
-TEST(QuickSort, Sort) {
-	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
-	}
-
-	SortFunctions::quickSort(v1);
-	std::sort(v2.begin(), v2.end());
-	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
-
-	v1 = { 1, 1, 1, 1 };
-	v2 = { 1, 1, 1, 1 };
+TEST_F(SortTest, QuickSort) {
 	SortFunctions::quickSort(v1);
 	std::sort(v2.begin(), v2.end());
 	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
-TEST(MergeSort, Sort) {
-	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
-	}
-
+TEST_F(SortTest, MergeSort) {
 	SortFunctions::mergeSort(v1);
 	std::sort(v2.begin(), v2.end());
 	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
-TEST(HeapSort, Sort) {
-	std::vector<int> v1, v2;
-	int val = 0;
-	for (int i = 0; i < SIZE; i++) {
-		val = getRandomNumber(MIN, MAX);
-		v1.push_back(val);
-		v2.push_back(val);
-	}
-
+TEST_F(SortTest, HeapSort) {
 	SortFunctions::heapSort(v1);
 	std::sort(v2.begin(), v2.end());
 	EXPECT_TRUE(std::equal(v1.begin(), v1.end(), v2.begin()));
